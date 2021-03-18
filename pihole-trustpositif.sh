2059b6422757f04931612a1990efa9efd4092b9f
iroelcat <38753552+iroelcat@users.noreply.github.com>
2021-03-18 20:55:25 +0700

    Update pihole-trustpositif.sh

--- pihole-trustpositif.sh
+++ pihole-trustpositif.sh
@@ -1,14 +1,15 @@
-#!/bin/bash 
-apt -y -q install dos2unix 
-rm -rf /tmp/domain.txt 
-curl -q -s "https://trustpositif.kominfo.go.id/assets/db/domains" | sed 's/\*.//g' | tr '[:upper:]' '[:lower:]' | sed '/\(\/\|:\|=\|\.\.\|\.$\|[0-9]\{1,3\}.[0-9]\{1,3\}.[0-9]\{1,3\}.[0-9]\{1,3\}\($\|\s\)\|^$\|\*\)/d' | perl -nle 'print if m{^[[:ascii:]]+$}' > /tmp/domain.txt; 
-dos2unix /tmp/domain.txt; 
-cat /tmp/domain.txt | sort | uniq > 
-/tmp/domain.txt.tmp 
-rm -rf /tmp/domain.txt 
-mv /tmp/domain.txt.tmp /tmp/domain.txt echo > /etc/dnsmasq.d/trustpositif.conf; IP=$(ifconfig | perl -nle 's/dr:(\S+)/print $1/e' | grep -v 127.0.0.1); for i in $(cat /tmp/domain.txt); 
-do 
-echo -e "address=/$i/$IP" >> 
-/etc/dnsmasq.d/trustpositif.conf; 
-done 
+#!/bin/bash
+apt -y -q install dos2unix
+rm -rf /tmp/domain.txt
+curl -q -s "https://trustpositif.kominfo.go.id/assets/db/domains" | sed 's/\*.//g' | tr '[:upper:]' '[:lower:]' | sed '/\(\/\|:\|=\|\.\.\|\.$\|[0-9]\{1,3\}.[0-9]\{1,3\}.[0-9]\{1,3\}.[0-9]\{1,3\}\($\|\s\)\|^$\|\*\)/d' | perl -nle 'print if m{^[[:ascii:]]+$}' > /tmp/domain.txt;
+dos2unix /tmp/domain.txt;
+cat /tmp/domain.txt | sort | uniq > /tmp/domain.txt.tmp
+rm -rf /tmp/domain.txt
+mv /tmp/domain.txt.tmp /tmp/domain.txt
+echo > /etc/dnsmasq.d/trustpositif.conf;
+IP=$(ifconfig | perl -nle 's/dr:(\S+)/print $1/e' | grep -v 127.0.0.1);
+for i in $(cat /tmp/domain.txt);
+do
+echo -e "address=/$i/$IP" >> /etc/dnsmasq.d/trustpositif.conf;
+done
 service dnsmasq restart
