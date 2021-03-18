b398b4e36d122746deb92c1c7a765e8510b79c17
iroelcat <38753552+iroelcat@users.noreply.github.com>
2021-03-18 22:19:03 +0700

    Create pihole-up.sh

---
+++ pihole-up.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+pihole disable
+service pihole-FTL stop
+rm /etc/pihole/pihole-FTL.log
+rm /var/log/pihole-FTL.log
+rm /var/log/pihole.log
+service pihole-FTL start
+pihole -f
+pihole arpflush
+pihole enable >
+done
